09a99c4991ca07d408b543ec07e6d75fbccca04c
Vlczakos <david@vlcak.com>
2025-06-12 14:18:58 +0200

    update

---
+++ webpage/script.js
@@ -0,0 +1,13 @@
+loadPage("home.html")
+
+function loadPage(url) {
+  fetch(url)
+    .then(response => response.text())
+    .then(html => {
+      document.getElementById("content").innerHTML = html;
+    })
+    .catch(error => {
+      document.getElementById("content").innerHTML = "<p>Chyba při načítání stránky.</p>";
+      console.error("Chyba:", error);
+    });
+}
